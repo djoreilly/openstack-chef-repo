2902e09bc8a7f52ef3c778cf7185935bb9999fa8
Hendrik Volkmer <h.volkmer@cloudbau.de>
2012-11-21 12:06:30 +0100

    Change environment so that it can be used with vagrant

--- environments/production.rb
+++ environments/production.rb
@@ -1,13 +1,13 @@
 name "production"
 description "Defines the network and database settings you're going to use with OpenStack. The networks will be used in the libraries provided by the osops-utils cookbook. This example is for FlatDHCP with 2 physical networks."
-
+# "precise" => "http://hypnotoad/precise-server-cloudimg-amd64.tar.gz"
 override_attributes(
   "glance" => {
     "image_upload" => true,
-    "images" => ["precise","cirros"],
+    "images" => ["cirros","precise"],
     "image" => {
-      "cirros" => "http://hypnotoad/cirros-0.3.0-x86_64-disk.img",
-      "precise" => "http://hypnotoad/precise-server-cloudimg-amd64.tar.gz"
+      "cirros" => "https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img",
+      "precise"=> "https://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-amd64.tar.gz"
     }
   },
   "mysql" => {
@@ -16,13 +16,16 @@ override_attributes(
   },
   "osops_networks" => {
     "public" => "10.0.111.0/24",
-    "management" => "10.0.111.0/24",
+    "management" => "192.168.10.0/24",
     "nova" => "10.0.111.0/24"
   },
   "nova" => {
+    "libvirt" => {
+      "virt_type" => "qemu"
+    },
     "network" => {
       "fixed_range" => "192.168.100.0/24",
-      "public_interface" => "eth0"
+      "public_interface" => "eth1"
     },
     "networks" => [
       {
@@ -31,7 +34,7 @@ override_attributes(
         "num_networks" => "1",
         "network_size" => "255",
         "bridge" => "br100",
-        "bridge_dev" => "eth0",
+        "bridge_dev" => "eth1",
         "dns1" => "8.8.8.8",
         "dns2" => "8.8.4.4"
       }
